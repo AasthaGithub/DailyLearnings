b594d2312761488eb10a1d70bf28dc6266bd44c4
AasthaGithub <49694058+AasthaGithub@users.noreply.github.com>
2020-05-14 15:54:39 +0530

    day 14 trie dict, set updated

--- leetcode/may_challenge/day14_prefix_trie.py
+++ leetcode/may_challenge/day14_prefix_trie.py
@@ -127,3 +127,39 @@ class Trie:
                 return False
             t = t[w]
         return True
+'''
+lightest
+'''
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.prefix_set = set()
+        self.element_set = set()
+        
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        if word:
+            if word not in self.element_set:
+                self.element_set.add(word)
+                for i in range(len(word)+1):
+                    self.prefix_set.add(word[0:i])
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        return word in self.element_set
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        return prefix in self.prefix_set
