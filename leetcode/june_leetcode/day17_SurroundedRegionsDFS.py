b3f895015237c73f61c2926abe05c75a06d8eab4
AasthaGithub <49694058+AasthaGithub@users.noreply.github.com>
2020-06-17 23:35:27 +0530

    day 17 updated

--- leetcode/june_leetcode/day17_SurroundedRegionsDFS.py
+++ leetcode/june_leetcode/day17_SurroundedRegionsDFS.py
@@ -170,3 +170,33 @@ class Solution:
                     row[i] = 'O'
                 else:
                     row[i] = 'X'
+                    
+#lightest soln
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        if not board or not board[0]: return
+        m, n = len(board), len(board[0])
+        from collections import deque
+
+        queue = deque()
+        for i in range(m):
+            if board[i][0] == "O": queue.append((i, 0))
+            if board[i][n-1] == "O": queue.append((i, n-1))
+        for j in range(1, n):
+            if board[0][j] == "O": queue.append((0, j))
+            if board[m-1][j] == "O": queue.append((m-1, j))
+
+        while queue:
+            i, j = queue.popleft()
+            board[i][j] = "B"
+            for p, q in ((i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)):
+                if 0 <= p < m and 0 <= q < n and board[p][q] == "O":
+                    queue.append((p, q))
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == "O": board[i][j] = "X"
+                if board[i][j] == "B": board[i][j] = "O"
