d0c3b6da269a57611e7259897c67e2cbac7adb35
AasthaGithub <49694058+AasthaGithub@users.noreply.github.com>
2020-06-22 17:38:19 +0530

    day 22 updated

--- leetcode/june_leetcode/day22_SingleNumberII.py
+++ leetcode/june_leetcode/day22_SingleNumberII.py
@@ -21,52 +21,30 @@ class Solution:
 #masking approach
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        # counter
-        # from collections import Counter
-        # for k, v in Counter(nums).items():
-        #     if v == 1:
-        #         return k
+           def singleNumber(self, nums: List[int]) -> int:
+        one = two = 0
         
-        # math
-        return int((sum(set(nums))*3-sum(nums))/2)
-    
-        # bit
-        ones = twos =threes = 0
-        for i in range(len(num)):
-            twos |= ones & nums[i]
-            ones ^= nums[i]
-            threes = twos & ones
+        for num in nums:
+            one = ~two & (one ^ num)
+            two = ~one & (two ^ num)
             
-            # if appear three times doing this will clear ones and twos
-            ones &= ~threes
-            twos &= ~threes
-            
-        return ones
+        return one
 
 #masking alternative
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-
-        single_num = 0
-        
+        single_num = 0        
         # compute single number by bit masking
-        for bit_shift in range(32):
-            
-            sum = 0
-            
-            for number in nums:
-                
+        for bit_shift in range(32):            
+            sum = 0            
+            for number in nums:                
                 # collect the bit sum
                 sum += ( number >> bit_shift ) & 1
-
             # Extract bit information of single number by modulo
             # Other number's bit sum is removed by mod 3 (i.e., all other numbers appear three times)
             single_num |= ( sum % 3 ) << bit_shift
-            
-            
-        
         if ( single_num & (1 << 31) ) == 0:
             return single_num
         else:
-			# handle for negative number
+		# handle for negative number
             return -( (single_num^(0xFFFF_FFFF))+1 )
